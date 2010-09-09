7889d7f0313d0de58e65546f8fdd2095a79b2627
cmsbuild <>
2010-09-09 14:26:21 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- HLTanalyzers-V01-20-24'.
    Sprout from HLTrigger-HLTanalyzers-BV03-02-29 2010-09-09 12:17:36 UTC Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch> 'More updates for running with 2E31 menu configuration'
    Cherrypick from master 2010-09-09 14:26:12 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Add missing include needed for 39X (based on V03-02-31 HLTrigger/HLTanalyzers)':
        interface/HLTAnalyzer.h

--- interface/HLTAnalyzer.h
+++ interface/HLTAnalyzer.h
@@ -18,6 +18,8 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
+#include "FWCore/ParameterSet/interface/Registry.h"
+
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "Geometry/CaloEventSetup/interface/CaloTopologyRecord.h"  
 
